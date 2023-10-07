3c31c17291d5ce90c3a96320f96c2bb023f83342
dasom222g <dasom228@gmail.com>
2023-10-07 16:51:39 +0900

    Add style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mood-gray-700': '#383737',
+      },
+    },
   },
   plugins: [],
 };
